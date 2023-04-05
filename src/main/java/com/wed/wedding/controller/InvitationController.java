cc7d93801c32af323a2f25c1e697a48ddcd9f93a
juiary-p <wsw9345@gmail.com>
2023-04-05 22:05:42 +0900

    - Controller 추가 - port 설정 - api test 코드

---
+++ src/main/java/com/wed/wedding/controller/InvitationController.java
@@ -0,0 +1,9 @@
+package com.wed.wedding.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/invite")
+public class InvitationController {
+}
